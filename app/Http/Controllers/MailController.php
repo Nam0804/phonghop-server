9c20b462ce04da52f395e00a5a632e645cd0538f
trungdq2407 <trungdq2407@gmail.com>
2023-11-28 14:10:52 +0700

    queue mail job done

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -3,23 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Jobs\SendEmailJob;
-use App\Mail\SendMail;
-use Carbon\Carbon;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Mail;
-use Nette\Utils\DateTime;
-use Symfony\Component\Process\Process;
 
 class MailController extends Controller
 {
-    protected function sendMail(Request $request)
+    protected function sendMail(): \Illuminate\Process\InvokedProcess
     {
-        $mytime = Carbon::now();
-        $meetingTimeInAdvance = Meeting::getTime();
-        if ($meetingTimeInAdvance == $mytime){
+//        $mytime = Carbon::now();
+//        $meetingTimeInAdvance = Meeting::getTime();
+//        if ($meetingTimeInAdvance == $mytime){
             $emailJob = new SendEmailJob();
             dispatch($emailJob);
             return \Illuminate\Support\Facades\Process::timeout(120)->start('php artisan queue:work');
-        }
+//        }
     }
 }
