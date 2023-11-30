1d3d77051a4b8153c8e007888dac6242fb1f2eef
Donald Bui <donald@vitex.asia>
2023-11-30 11:08:57 +0700

    update admin repository

--- app/Repository/AdminRepository/BaseAdminRepository.php
+++ app/Repository/AdminRepository/BaseAdminRepository.php
@@ -1,15 +1,15 @@
 <?php
 namespace App\Repository\AdminRepository;
 
-use App\Models\User;
+use App\Models\Admin;
 use Illuminate\Pagination\LengthAwarePaginator;
 
 interface BaseAdminRepository
 {
     public function list(): LengthAwarePaginator;
-    public function create(array $data): User;
+    public function create(array $data): Admin;
     public function update(array $data, string $id): bool;
     public function delete(string $id): bool;
-    public function show(string $id): User;
+    public function show(string $id): Admin;
 
 }
