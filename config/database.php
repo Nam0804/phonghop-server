4e27e24e180518b54dd324e38d54f039938ba328
Billy <billy@vitex.asia>
2023-12-18 11:34:56 +0700

    Small changes

--- config/database.php
+++ config/database.php
@@ -61,9 +61,6 @@ return [
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
-            'options' => [
-                \Mnabialek\LaravelSqlLogger\Providers\Bindings\BindLogger::class,
-            ],
         ],
 
         'pgsql' => [
